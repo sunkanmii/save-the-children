b52d9b20c0be79823c5b2d12c22ff97283bd03c3
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2020-08-19 22:07:59 +0100

    nav working

--- js/index.js
+++ js/index.js
@@ -5,13 +5,11 @@ const breadCrumbs = document.querySelectorAll(".bread-crumb");
 const mainNav = document.querySelector("header .section-links");
 
 const DisplayNav = () => {
-    if(mainNav.style.visibility === "hidden"){
+    if(mainNav.style.display === "none"){
         mainNav.style.display = "inline-block";
-        mainNav.style.visibility = "visible";
     }
     else{
         mainNav.style.display = "none";
-        mainNav.style.visibility = "hidden";
     };
 }
 
